02806c7c5bf359c80adb7bae08db645cb5cd3c61
gstadtler <ghstadtler@gmail.com>
2020-11-10 00:57:35 -0300

    corrigindo bugs de inicialização do servidor

--- config/app.js
+++ config/app.js
@@ -27,7 +27,7 @@ module.exports = {
   | to encrypt cookies, sessions and other sensitive data.
   |
   */
-  appKey: Env.getOrFail('APP_KEY'),
+  appKey: Env.getOrFail('APP_KEY', 'bCSV1wfYjMT7Rm2fNd3r08hWGCGMwT0y'),
 
   http: {
     /*
